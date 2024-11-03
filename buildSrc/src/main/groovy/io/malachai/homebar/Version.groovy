5ba6b8b5b3cbb567770301262fe48409b8a2f346
Taehee Lee <prussian1933@naver.com>
2024-11-03 19:36:35 +0900

    [Init] application.yaml 배제, 모듈 추가

--- buildSrc/src/main/groovy/io/malachai/homebar/Version.groovy
+++ buildSrc/src/main/groovy/io/malachai/homebar/Version.groovy
@@ -2,8 +2,12 @@ package io.malachai.homebar
 
 class Version {
 
+    static String JAXB_VERSION = '4.0.1'
+
     static String MYBATIS_SPRING_VERSION = '3.0.3'
     static String HIBERNATE_VERSION = '6.2.0.Final'
+    static String JWT_VERSION = '0.9.1'
+    static String MYSQL_VERSION = '8.2.0'
 
 }
 
